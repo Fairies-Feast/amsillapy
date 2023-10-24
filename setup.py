cfba7cedf9092f65d62667af7fae0ed5789d5193
Fairies0feast on Replit <86023563+Fairies-Feast@users.noreply.github.com>
2023-10-24 18:33:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 from my_pip_package import __version__
 
 setup(
-    name='my_pip_package',
+    name='amsillapy',
     version=__version__,
 
-    url='https://github.com/MichaelKim0407/tutorial-pip-package',
-    author='Michael Kim',
-    author_email='mkim0407@gmail.com',
+    url='https://github.com/Fairies-Feast/amsillapy',
+    author='Will Caesar',
+    author_email='info@ffm.pw',
 
-    py_modules=['my_pip_package'],
+    py_modules=['requests'],
 )
