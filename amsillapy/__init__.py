714dc67b675cd0650b0caa0627b6fc67c68dbe84
Fairies0feast on Replit <86023563+Fairies-Feast@users.noreply.github.com>
2023-10-26 20:36:35 -0500

    Add isOnReplit and didUserTip

--- amsillapy/__init__.py
+++ amsillapy/__init__.py
@@ -1,2 +1,27 @@
-def add(num1,num2):
-  return num1+num2
+import requests
+import os
+
+def isOnReplit():
+  if os.environ['REPL_OWNER'] and os.environ["REPL_ID"] and os.environ["REPL_OWNER_ID"]:
+    return True
+  else:
+    return False
+
+def didUserTip(thisReplURL):
+  if not isOnReplit():
+    print("(AmsillaPy Exceptions)")
+    print("!     EXCEPTION     !")
+    print("Code:   NOT_ON_REPLIT")
+    return
+  try:
+    raw = requests.get("httos://tipping.repl.co/")
+  except:
+    print("(AmsillaPy Exceptions)")
+    print("!       ERROR        !")
+    print("Code: FETCH_API_CANCEL")
+    quit()
+  raw = raw.text
+  if os.environ["REPL_OWNER_ID"] in raw:
+    return True
+  else:
+    return False
